edec8100643d35c237e2309288c849abf134c1b8
shiro-mail <shiro736@gmail.com>
2025-08-15 18:34:23 +0900

    PORT変更

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def about():
     return '<h1>About</h1><p>これはFlaskの練習用アプリケーションです。</p>'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
